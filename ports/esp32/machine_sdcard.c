341bb5e0ff16d6674b236081d743d0e6736a0a84
姚建忠 <hunt-man@163.com>
2024-07-26 16:55:38 +0800

    增加SDMMC卡指定GPIO

--- ports/esp32/machine_sdcard.c
+++ ports/esp32/machine_sdcard.c
@@ -183,8 +183,8 @@ static mp_obj_t machine_sdcard_make_new(const mp_obj_type_t *type, size_t n_args
         ARG_d0,
         ARG_d1,
         ARG_d2,
-        ARG_d3
-        ARG_freq,
+        ARG_d3,
+        ARG_freq
     };
     static const mp_arg_t allowed_args[] = {
         { MP_QSTR_slot,     MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = 1} },
